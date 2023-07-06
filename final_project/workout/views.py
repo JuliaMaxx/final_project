d4aed7850b6d6deaf1e11636e9c50a40e0176497
Julia Maxy <jjuliamaxxx@gmail.com>
2023-07-06 07:42:07 +0300

    settings

--- final_project/workout/views.py
+++ final_project/workout/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request, 'workout/index.html')
