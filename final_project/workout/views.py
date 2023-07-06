20059349ee914ac8ee8dbcf4c853015a9a7af40a
Julia Maxy <jjuliamaxxx@gmail.com>
2023-07-06 11:38:11 +0300

    authentication

--- final_project/workout/views.py
+++ final_project/workout/views.py
@@ -4,6 +4,56 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from .models import User
+from django.db import IntegrityError
 
 def index(request):
     return render(request, 'workout/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "workout/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "workout/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "workout/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username,password)
+            user.save()
+        except IntegrityError:
+            return render(request, "workout/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "workout/register.html")
